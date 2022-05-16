28d7e3c00b6b43f048bad9bc892f528717714412
Senem Isik <77026721+SENEMISIK@users.noreply.github.com>
2022-05-16 14:37:12 -0700

    prinshape

--- util.py
+++ util.py
@@ -59,6 +59,7 @@ def crop_like(input, target):
 
 def flow2rgb(flow_map, max_value):
     flow_map_np = flow_map.detach().cpu().numpy()
+    print(flow_map_np.shape)
     _, h, w = flow_map_np.shape
     flow_map_np[:,(flow_map_np[0] == 0) & (flow_map_np[1] == 0)] = float('nan')
     rgb_map = np.ones((3,h,w)).astype(np.float32)
