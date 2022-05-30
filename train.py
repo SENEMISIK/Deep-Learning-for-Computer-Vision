e512eef910171c4d52b734d35d8c0bd4cf0aaf58
Ubuntu <ubuntu@ip-172-31-40-212.us-west-2.compute.internal>
2022-05-30 21:01:56 +0000

    data aug and pretrain option

--- train.py
+++ train.py
@@ -4,21 +4,23 @@ from train_helper import *
 parser = argparse.ArgumentParser(description='Train and evaluate optical flow model.')
 parser.add_argument("-m", "--model-type", default="flownet", type=str)
 parser.add_argument("-s", "--size", default=1000, type=int)
-parser.add_argument("-p", "--pretrain", default=True, type=bool)
+parser.add_argument("-p", "--pretrain", default=1, type=int)
+parser.add_argument("-a", "--augment", default=1, type=int)
 
 def main():
     args = parser.parse_args()
     pretrain_size = args.size
     model_type = args.model_type
     finetune_size = 520
-    pretrain = args.pretrain
+    pretrain = bool(args.pretrain)
+    augment = bool(args.augment)
     
-    train_loader, test_loader, fc_loader = get_data(pretrain_size, finetune_size)
+    train_loader, test_loader, fc_loader = get_data(pretrain_size, finetune_size, augment)
     
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     
     if model_type == "flownet":
-        model, pretrain_losses, finetune_losses = train_flownet(fc_loader, train_loader, device, pretrain)
+        model, pretrain_losses, finetune_losses = train_flownet(fc_loader, train_loader, device, augment, pretrain)
         epe, f1 = test_flownet(model, test_loader, device)
         print("Test:")
         print("Epe: ", epe)
@@ -31,11 +33,11 @@ def main():
         }
         if not pretrain:
             pretrain_size = 0
-        save_results(results, model_type, pretrain_size)
-        save_model(model, model_type, pretrain_size)
+        save_results(results, model_type, pretrain_size, augment)
+        save_model(model, model_type, pretrain_size, augment)
     
     elif model_type == "raft":
-        model, pretrain_losses, finetune_losses = train_raft(fc_loader, train_loader, device, pretrain)
+        model, pretrain_losses, finetune_losses = train_raft(fc_loader, train_loader, device, augment, pretrain)
         epe, f1 = test_raft(model, test_loader, device)
         print("Test:")
         print("Epe: ", epe)
@@ -48,8 +50,8 @@ def main():
         }
         if not pretrain:
             pretrain_size = 0
-        save_results(results, model_type, pretrain_size)
-        save_model(model, model_type, pretrain_size)
+        save_results(results, model_type, pretrain_size, augment)
+        save_model(model, model_type, pretrain_size, augment)
     else:
         raise(Exception("Invalid model type"))
     
--- train_helper.py
+++ train_helper.py
@@ -8,16 +8,25 @@ from multiscaleloss import multiscaleEPE, realEPE
 import time
 import os
 
-def get_data(pretrain_size, finetune_size):
+def get_data(pretrain_size, finetune_size, augment):
+    
+    np.random.seed(17)
+    
     fc_transforms = OpticalFlowPresetTrain(crop_size=(368, 496), min_scale=0.1, max_scale=1.0, do_flip=True)
+    if not augment:
+        fc_transforms = OpticalFlowPresetEval()
     flying_chairs = torchvision.datasets.FlyingChairs(root=".", split="train", transforms=fc_transforms)
+    
     s_transforms = OpticalFlowPresetTrain(crop_size=(368, 768), min_scale=-0.2, max_scale=0.6, do_flip=True)
+    if not augment:
+        s_transforms = OpticalFlowPresetEval()
     sintel_train = torchvision.datasets.Sintel(root=".", split="train", pass_name="clean", transforms=s_transforms)
+    
     test_transforms = OpticalFlowPresetEval()
     sintel_test = torchvision.datasets.Sintel(root=".", split="train", pass_name="clean", transforms=test_transforms)
 
-    train_ind = np.random.choice(len(sintel_train), finetune_size, replace=False)
-    test_ind = np.array(list(set(range(len(sintel_train))) - set(train_ind)))
+    train_ind = np.random.choice(len(sintel_train), finetune_size, replace=False)[:10]
+    test_ind = np.array(list(set(range(len(sintel_train))) - set(train_ind)))[:10]
     fc_ind = np.random.choice(len(flying_chairs), pretrain_size, replace=False)
 
     sintel_train = torch.utils.data.Subset(sintel_train, train_ind)
@@ -30,13 +39,17 @@ def get_data(pretrain_size, finetune_size):
 
     return train_loader, test_loader, fc_loader
 
-def train_flownet_one_epoch(model, train_loader, optimizer, scheduler, device, epoch):
+def train_flownet_one_epoch(model, train_loader, optimizer, scheduler, device, augment, epoch):
     start = time.time()
     epoch_loss = 0.0
     for i, data_blob in enumerate(train_loader):
         optimizer.zero_grad()
         
-        image1, image2, flow_gt, valid_flow_mask = (x.to(device) for x in data_blob)
+        if augment:
+            image1, image2, flow_gt, valid_flow_mask = (x.to(device) for x in data_blob)
+        else:
+            image1, image2, flow_gt = (x.to(device) for x in data_blob)
+            
         image = torch.cat((image1, image2), dim=1)
         
         output = model(image)
@@ -57,13 +70,24 @@ def train_flownet_one_epoch(model, train_loader, optimizer, scheduler, device, e
     print("Epoch", epoch + 1, "finished in", round(time.time() - start, 1), "seconds. Loss:", epoch_loss)
     return epoch_loss
 
-def train_raft_one_epoch(model, train_loader, optimizer, scheduler, device, epoch, num_train_flow_updates):
+def train_raft_one_epoch(model, train_loader, optimizer, scheduler, device, epoch, augment, num_train_flow_updates):
     start = time.time()
     epoch_loss = 0.0
     for i, data_blob in enumerate(train_loader):
         optimizer.zero_grad()
 
-        image1, image2, flow_gt, valid_flow_mask = (x.to(device) for x in data_blob)
+        if augment:
+            image1, image2, flow_gt, valid_flow_mask = (x.to(device) for x in data_blob)
+        else:
+            size = (800, 400)
+            resize = torchvision.transforms.Resize(size)
+            image1, image2, flow_gt = (x.to(device) for x in data_blob)
+            image1 = resize(image1)
+            image2 = resize(image2)
+            flow_gt = torch.nn.functional.interpolate(flow_gt, size, mode="bilinear") 
+            mask_shape = [flow_gt.shape[0], flow_gt.shape[2], flow_gt.shape[3]]
+            valid_flow_mask = torch.ones(mask_shape).type(torch.bool).to(device)
+            
         flow_predictions = model(image1, image2, num_flow_updates=num_train_flow_updates)
 
         loss = sequence_loss(flow_predictions, flow_gt, valid_flow_mask)
@@ -81,9 +105,9 @@ def train_raft_one_epoch(model, train_loader, optimizer, scheduler, device, epoc
     print("Epoch", epoch + 1, "finished in", round(time.time() - start, 1), "seconds. Loss:", epoch_loss)
     return epoch_loss
 
-def train_flownet(fc_loader, train_loader, device, pretrain=True):
-    pretrain_epochs = 100
-    finetune_epochs = 20
+def train_flownet(fc_loader, train_loader, device, augment, pretrain=True):
+    pretrain_epochs = 10
+    finetune_epochs = 5
     lr = 1e-4
     weight_decay = 4e-4
     model = FlowNetS()
@@ -97,17 +121,17 @@ def train_flownet(fc_loader, train_loader, device, pretrain=True):
     if pretrain:
         print("Pretraining FlowNet on", len(fc_loader.dataset), "FlyingChairs examples...")
         for epoch in range(pretrain_epochs):
-            pretrain_losses.append(train_flownet_one_epoch(model, fc_loader, optimizer, scheduler, device, epoch))
+            pretrain_losses.append(train_flownet_one_epoch(model, fc_loader, optimizer, scheduler, device, augment, epoch))
     print("Finetuning Flownet on", len(train_loader.dataset), "Sintel examples...")
     for epoch in range(finetune_epochs):
-        finetune_losses.append(train_flownet_one_epoch(model, train_loader, optimizer, scheduler, device, epoch))
+        finetune_losses.append(train_flownet_one_epoch(model, train_loader, optimizer, scheduler, device, augment, epoch))
     return model, pretrain_losses, finetune_losses
 
-def train_raft(fc_loader, train_loader, device, pretrain=True):
+def train_raft(fc_loader, train_loader, device, augment, pretrain=True):
     model = torchvision.models.optical_flow.raft_small()
     model.to(device)
     pretrain_epochs = 20
-    finetune_epochs = 10
+    finetune_epochs = 1
 
     lr = 2e-5
     weight_decay = 5e-5
@@ -134,10 +158,10 @@ def train_raft(fc_loader, train_loader, device, pretrain=True):
     if pretrain:
         print("Pretraining RAFT on", len(fc_loader.dataset), "FlyingChairs examples...")
         for epoch in range(pretrain_epochs):
-            pretrain_losses.append(train_raft_one_epoch(model, fc_loader, optimizer, scheduler, device, epoch, num_train_flow_updates))
+            pretrain_losses.append(train_raft_one_epoch(model, fc_loader, optimizer, scheduler, device, augment, epoch, num_train_flow_updates))
     print("Finetuning RAFT on", len(train_loader.dataset), "Sintel examples...")
     for epoch in range(finetune_epochs):
-        finetune_losses.append(train_raft_one_epoch(model, train_loader, optimizer, scheduler, device, epoch, num_train_flow_updates))
+        finetune_losses.append(train_raft_one_epoch(model, train_loader, optimizer, scheduler, device, augment, epoch, num_train_flow_updates))
     return model, pretrain_losses, finetune_losses
     
 def test_flownet(model, test_loader, device):
@@ -147,13 +171,13 @@ def test_flownet(model, test_loader, device):
 
     with torch.no_grad():
         for i, data_blob in enumerate(test_loader):
-            image1, image2, flow_gt, valid_flow_mask = (x.to(device) for x in data_blob)
+            image1, image2, flow_gt = (x.to(device) for x in data_blob)
             image = torch.cat((image1, image2), dim=1)
 
             output = model(image)
             h, w = flow_gt.size()[-2:]
             upsampled_output = torch.nn.functional.interpolate(output, (h,w), mode='bilinear', align_corners=False)
-            metrics, _ = compute_metrics(upsampled_output, flow_gt, valid_flow_mask)
+            metrics, _ = compute_metrics(upsampled_output, flow_gt)
             epe = metrics["epe"]
             f1 = metrics["f1"]
 
@@ -176,7 +200,7 @@ def test_raft(model, test_loader, device, num_test_updates=32):
             image1, image2, flow_gt = (x.to(device) for x in data_blob)
             padder = InputPadder(image1.shape)
             image1, image2 = padder.pad(image1, image2)
-
+   
             flow_predictions = model(image1, image2, num_flow_updates=num_test_updates)
             flow_pred = flow_predictions[-1]
             flow_pred = padder.unpad(flow_pred)
@@ -193,10 +217,12 @@ def test_raft(model, test_loader, device, num_test_updates=32):
     
     return total_epe, total_f1
 
-def save_results(results, model_type, pretrain_size):
-    path = os.path.join("results", model_type + str(pretrain_size) + "_results.txt")
+def save_results(results, model_type, pretrain_size, augment):
+    augment = "A" if augment else "NA"
+    path = os.path.join("results", model_type + str(pretrain_size) + augment + "_results.txt")
     torch.save(results, path)
     
-def save_model(model, model_type, pretrain_size):
-    path = os.path.join("results", model_type + str(pretrain_size) + "_params.pt")
+def save_model(model, model_type, pretrain_size, augment):
+    augment = "A" if augment else "NA"
+    path = os.path.join("results", model_type + str(pretrain_size) + augment + "_params.pt")
     torch.save(model.state_dict(), path)
